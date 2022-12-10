7e97c80d62b57bf398425932def07dc1d16913d2
Valeria177 <63343126+Valeria177@users.noreply.github.com>
2022-12-10 12:04:27 +0300

    Лаба 7-8

---
+++ ПРИ-118-ПКГ#08-Левченко/ПРИ118-ПКГ№08-Левченко/ПРИ118-ПКГ№08-Левченко/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ПРИ118_ПКГ_08_Левченко
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
